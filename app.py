3d73b9562871837ff662b9076a542246e833962d
amrShokir <amrisa1310@gmail.com>
2025-12-06 23:44:23 +0200

    FIx: fix the logout problem, added complicated profile page view

--- app.py
+++ app.py
@@ -16,10 +16,19 @@ app.register_blueprint(product_bp)
 app.register_blueprint(cart_bp)
 app.register_blueprint(order_bp)
 
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 
 @app.route('/')
 def index():
     return redirect(url_for('product_routes.list_products'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

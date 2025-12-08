56755b6282465e2037129f690b6c45163b631732
amrShokir <amrisa1310@gmail.com>
2025-12-08 08:01:09 +0200

    FIX nothing in particular just imporving the code

--- models/order_item_model.py
+++ models/order_item_model.py
@@ -3,7 +3,6 @@
 from .base_model import CSVModel
 
 class OrderItemModel(CSVModel):
-    """Manages data from order_items.csv"""
     def __init__(self):
         super().__init__('order_items')
         self.primary_key = 'order_item_id'
